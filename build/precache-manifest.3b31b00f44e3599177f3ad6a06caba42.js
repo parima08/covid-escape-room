294c81e9dd4c225ab3beaa86c90b61a81779839c
Parima <parima@openspace.ai>
2020-05-21 22:28:28 -0700

    Added github pages

---
+++ build/precache-manifest.3b31b00f44e3599177f3ad6a06caba42.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "497fea35b80b1448fc16e5efc5f3fd7d",
+    "url": "/design-studio/index.html"
+  },
+  {
+    "revision": "6d15e4067d4fc51eca75",
+    "url": "/design-studio/static/css/main.eb364759.chunk.css"
+  },
+  {
+    "revision": "edf50ef76576e96ca75c",
+    "url": "/design-studio/static/js/2.eb66816f.chunk.js"
+  },
+  {
+    "revision": "6d15e4067d4fc51eca75",
+    "url": "/design-studio/static/js/main.233da92b.chunk.js"
+  },
+  {
+    "revision": "9aa1c9472b123f40e67a",
+    "url": "/design-studio/static/js/runtime-main.a41b4cf2.js"
+  },
+  {
+    "revision": "01a77219ee40cfb91e7d16dbc21d3438",
+    "url": "/design-studio/static/media/clue1.01a77219.png"
+  }
+]);
